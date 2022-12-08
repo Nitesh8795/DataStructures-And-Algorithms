3821ecb0f5880870b9b7ebb8f2b0deefe44333be
Nitesh Kumar Singh <anshukumargautam095@gmail.com>
2022-12-08 17:27:49 +0530

    Arrays Concatenation Program

---
+++ Problem Solving(Codes)/Arrays/Concatenation_2.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Concatenation_2
+{
+    static int[] concatenate_func(int[] arr1)
+    {
+        int j=0;
+
+        int new_length = arr1.length;
+
+        int[] arr2 = new int[2*new_length];
+
+        for(int i=0; i<arr1.length; i++)
+        {
+            arr2[i] = arr1[i];
+        }
+
+        for(int i=arr1.length; i<arr2.length; i++)
+        {
+            arr2[i] = arr1[j];
+            j++;
+        }
+
+        return arr2;
+    }
+
+
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the length of array : ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of array : ");
+
+        for(int i=0; i<n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        sc.close();
+
+        int[] arr1 = concatenate_func(arr);
+
+        System.out.println("Old Array : " + Arrays.toString(arr));
+        System.out.println("New Array : " + Arrays.toString(arr1));
+        
+    }
+}
